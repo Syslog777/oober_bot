5243a7d0c8dfc290a95318cb010d2c1406e3e5a5
Charles Fox <35052672+CharFox1@users.noreply.github.com>
2022-03-09 10:17:21 -0500

    Update global_rules.py

--- global_rules.py
+++ global_rules.py
@@ -4,6 +4,10 @@ def run(data, bot_info, send, lineIndex):
     random.seed()
     f = open("FamilyFeudData.csv", "r", encoding="utf-8")
     lines = f.readlines()
+    f.close()
+    f = open("lineIndex.txt", "r", encoding="utf-8")
+    lineIndex = int(f.readline().strip()[0])
+    f.close()
 
     help_message = "Help:\n.help  -->  This screen\n.test  -->  Try it!\nOtherwise, repeats your message."
 
@@ -17,8 +21,11 @@ def run(data, bot_info, send, lineIndex):
         send("Hi there! Your bot is working, you should start customizing it now.", bot_info[0])
         return True
     
-    if message == "next":
+    if message == ".next":
         lineIndex = random.randint(lines)
+        f = open("lineIndex.txt", "w", encoding="utf-8")
+        f.write(lineIndex)
+        f.close()
         randomLine = lines[lineIndex].strip().split(",")
         prompt = randomLine[0]
         answers = []
@@ -31,7 +38,25 @@ def run(data, bot_info, send, lineIndex):
         #print(answers)
         msg = prompt
         send(msg, bot_info[0])
-        return lineIndex
+        
+    randomLine = lines[lineIndex].strip().split(",")
+    prompt = randomLine[0]
+    answers = {}
+    i = 1
+    while i in range(len(randomLine)-1):
+        answers[randomLine[i].strip().lower()] = randomLine[i+1]
+        i += 2
+    
+    messageRaw = message
+    message = message.strip().lower()
+    if message in answers.keys():
+        score = answers[message]
+        send("Congratulations! {} is correct! {} points!".format(messageRaw, score), bot_info[0])
+        
+    else:
+        # sorry that's incorrect
+        send("Sorry that's not right", bot_info[0])
+        
 
     #send("Hi {}! You said: {}".format(data['name'], data['text']), bot_info[0])
     return True
