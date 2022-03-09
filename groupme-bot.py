60d965d8c42113b4b25c40405ca0e25e4776028f
Charles Fox <35052672+CharFox1@users.noreply.github.com>
2022-03-09 00:21:59 -0500

    Update groupme-bot.py

--- groupme-bot.py
+++ groupme-bot.py
@@ -119,9 +119,9 @@ def webhook():
         return "ok", 200
 
     if data['group_id'] in GROUP_RULES:
-        if GROUP_RULES[data['group_id']].run(data, BOT_INFO[data['group_id']], send_message):
+        if GROUP_RULES[data['group_id']].run(data, BOT_INFO[data['group_id']], send_message, curLine):
             return "ok", 200
 
-    curLine = GLOBAL_RULES.run(data, BOT_INFO[data['group_id']], send_message)
+    curLine = GLOBAL_RULES.run(data, BOT_INFO[data['group_id']], send_message, curLine)
 
     return "ok", 200
