677c7669187da7e0df119b49c754e54528630675
Charles Fox <35052672+CharFox1@users.noreply.github.com>
2022-03-08 23:54:32 -0500

    Update global_rules.py

--- global_rules.py
+++ global_rules.py
@@ -1,4 +1,9 @@
 def run(data, bot_info, send):
+    
+    import random
+    random.seed()
+    f = open("FamilyFeudData.csv", "r", encoding="utf-8")
+    lines = f.readlines()
 
     help_message = "Help:\n.help  -->  This screen\n.test  -->  Try it!\nOtherwise, repeats your message."
 
@@ -12,8 +17,8 @@ def run(data, bot_info, send):
         send("Hi there! Your bot is working, you should start customizing it now.", bot_info[0])
         return True
     
-    if message == '123':
-        send("456", bot_info[0])
+    if message == "next":
+        
 
-    send("Hi {}! You said: {}".format(data['name'], data['text']), bot_info[0])
+    #send("Hi {}! You said: {}".format(data['name'], data['text']), bot_info[0])
     return True
