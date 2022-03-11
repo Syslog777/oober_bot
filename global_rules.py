f7a591bbd96f56ca98937f54278cd25be10af23f
Charles Fox <35052672+CharFox1@users.noreply.github.com>
2022-03-11 09:54:58 -0500

    Update global_rules.py

--- global_rules.py
+++ global_rules.py
@@ -68,7 +68,7 @@ def run(data, bot_info, send):
                 prompt.append(randomLine[-(i)])
                 i += 1
         prompt = ", ".join(prompt)
-        print(prompt)
+        print("prompt =", prompt)
         msg = prompt
         send(msg, bot_info[0])
         
