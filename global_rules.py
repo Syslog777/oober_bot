313a00c34ce1103a0106da70461790966dec3479
tito <tcodex250@gmail.com>
2022-03-15 16:10:50 -0500

    bugfix caused by sleep

--- global_rules.py
+++ global_rules.py
@@ -19,31 +19,31 @@ import time
 """
 def run(data, bot_info, send_message):
     if data['text'].lower() == 'wassup' + data['name']:
-        time.sleep(2)
+       # time.sleep(2)
         send_message('Yooo wsg fam!', bot_info[0])
         return True
     elif data['text'].lower().contains('@oober') or data['text'].lower().contains('@bot'):
-        time.sleep(2)
+       # time.sleep(2)
         send_message('Wassup?', bot_info[0])
         return True
     elif data['text'].lower() == 'i need a ride':
-        time.sleep(2)
+       # time.sleep(2)
         send_message('Ok, sending a DM to all ghetto oober drivers', bot_info[0])
         return True
     elif data['text'].lower() == '!test':
-        time.sleep(2)
+        #time.sleep(2)
         send_message('I am in test mode, my functionality is subject to change', bot_info[0])
         return True
     elif data['text'].lower() == '!request ride':
-        time.sleep(2)
+       # time.sleep(2)
         send_message('Sending a DM to all ghetto oober drivers. I am in test mode, my functionality is subject to change', bot_info[0])
         return True
     elif data['text'].lower() == '!report':
-        time.sleep(2)
+       # time.sleep(2)
         send_message('Sending you a link to our report form to your DMs. I am in test mode, my functionality is subject to change', bot_info[0])
         return True
     elif data['text'].lower() == 'catch me outside':
-        time.sleep(2)
+       # time.sleep(2)
         send_message('Aight bet!  I am in test mode, my functionality is subject to change', bot_info[0])
         return True
     else:
