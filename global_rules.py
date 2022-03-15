9d14974e12cff9be03af6ddcd1fb6b1d7492553f
tito <tcodex250@gmail.com>
2022-03-14 21:19:58 -0500

    quickfix

--- global_rules.py
+++ global_rules.py
@@ -2,10 +2,10 @@ def run(data, bot_info, send_message):
     if data['text'].lower() == 'wassup':
         send_message('Yooo wsg fam!', bot_info[0])
         return True
-    elif data['text'].lower() == 'Wassup':
+    elif data['text'].lower() == 'wassup':
         send_message('Yooo wsg fam!', bot_info[0])
         return True
-    elif data['text'].lower() == 'Hru':
+    elif data['text'].lower() == 'hru':
         send_message('I am running without errors, my mind (CPU) is clear and I\'m feeling great! 😄', bot_info[0])
         return True
     elif data['text'].lower() == 'I need a ride':
@@ -20,7 +20,7 @@ def run(data, bot_info, send_message):
     elif data['text'].lower() == '!report':
         send_message('Sending you a link to our report form to your DMs. I am in test mode, my functionality is subject to change', bot_info[0])
         return True
-    elif data['text'].lower() == 'Catch me outside':
+    elif data['text'].lower() == 'catch me outside':
         send_message('Aight bet!  I am in test mode, my functionality is subject to change', bot_info[0])
         return True
     else:
